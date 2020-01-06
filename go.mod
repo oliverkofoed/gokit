452ddf189ba2b39006d052be00f19ecdd46df94f
Oliver <oliver@oliverkofoed.com>
2020-01-06 16:42:20 +0100

    renamed logkit.Context properties

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/oliverkofoed/gokit
+
+go 1.13
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/mattn/go-isatty v0.0.10
+	github.com/tdewolff/minify v2.3.6+incompatible
+	github.com/tdewolff/parse v2.3.4+incompatible // indirect
+)
--- logkit/output.go
+++ logkit/output.go
@@ -13,7 +13,7 @@ type Output interface {
 	Event(msg Event)
 }
 
-func printValues(w io.Writer, fields []Field) {
+func PrintValues(w io.Writer, fields []Field) {
 	var anyValues = false
 	for i, field := range fields {
 		if i == 0 {
@@ -25,46 +25,50 @@ func printValues(w io.Writer, fields []Field) {
 
 		io.WriteString(w, field.Key)
 		io.WriteString(w, ": ")
-		switch field.FieldType {
-		case FieldTypeString:
-			io.WriteString(w, field.Str)
-			break
-		case FieldTypeInt64:
-			io.WriteString(w, strconv.FormatInt(field.Integer, 10))
-			break
-		case FieldTypeBytes:
-			b := field.Value.([]byte)
-
-			if hex.EncodedLen(len(b)) > maxStringPrintLength {
-				io.WriteString(w, hex.EncodeToString(b[:maxStringPrintLength/2]))
-				io.WriteString(w, "...")
-			} else {
-				io.WriteString(w, hex.EncodeToString(b))
-			}
-			break
-		case FieldTypeStringer:
-			io.WriteString(w, field.Value.(fmt.Stringer).String())
-		case FieldTypeDuration:
-			fmt.Fprintf(w, "%v", time.Duration(field.Integer))
-		case FieldTypeTime:
-			fmt.Fprintf(w, "%v", field.Value)
-		case FieldTypeErr:
-			fmt.Fprintf(w, "%v", field.Value)
-			break
-		case FieldTypeBool:
-			if field.Integer == 1 {
-				io.WriteString(w, "true")
-			} else {
-				io.WriteString(w, "false")
-			}
-		case FieldTypeInterface:
-			fmt.Fprintf(w, "%v", field.Value)
-		default:
-			panic(fmt.Sprintf("unknown field type: %v", field.FieldType))
-		}
+		PrintValue(w, field)
 	}
 	if anyValues {
 		io.WriteString(w, ")")
+	}
+}
+
+func PrintValue(w io.Writer, field Field) {
+	switch field.FieldType {
+	case FieldTypeString:
+		io.WriteString(w, field.Str)
+		break
+	case FieldTypeInt64:
+		io.WriteString(w, strconv.FormatInt(field.Integer, 10))
+		break
+	case FieldTypeBytes:
+		b := field.Value.([]byte)
+
+		if hex.EncodedLen(len(b)) > maxStringPrintLength {
+			io.WriteString(w, hex.EncodeToString(b[:maxStringPrintLength/2]))
+			io.WriteString(w, "...")
+		} else {
+			io.WriteString(w, hex.EncodeToString(b))
+		}
+		break
+	case FieldTypeStringer:
+		io.WriteString(w, field.Value.(fmt.Stringer).String())
+	case FieldTypeDuration:
+		fmt.Fprintf(w, "%v", time.Duration(field.Integer))
+	case FieldTypeTime:
+		fmt.Fprintf(w, "%v", field.Value)
+	case FieldTypeErr:
+		fmt.Fprintf(w, "%v", field.Value)
+		break
+	case FieldTypeBool:
+		if field.Integer == 1 {
+			io.WriteString(w, "true")
+		} else {
+			io.WriteString(w, "false")
+		}
+	case FieldTypeInterface:
+		fmt.Fprintf(w, "%v", field.Value)
+	default:
+		panic(fmt.Sprintf("unknown field type: %v", field.FieldType))
 	}
 }
 
