052365db8afefb463df0a01c0fb20983e31dc2fc
Oliver <oliver@oliverkofoed.com>
2021-04-21 12:23:14 +0200

    filestorekit: better serving of zipped media

--- filestorekit/mediastore.go
+++ filestorekit/mediastore.go
@@ -2,6 +2,7 @@ package filestorekit
 
 import (
 	"bytes"
+	"compress/gzip"
 	"context"
 	"errors"
 	"fmt"
@@ -27,19 +28,20 @@ func NewMedia(cache *cachekit.Cache, underlying Store) *MediaStore {
 	}
 }
 
-func (s *MediaStore) GetFormattedMedia(ctx context.Context, path string, format string) (content []byte, contentType string, err error) {
+func (s *MediaStore) GetFormattedMedia(ctx context.Context, path string, format string, gzipContent bool) (content []byte, contentType string, zipped bool, err error) {
 	// check cache
-	cacheKey := []byte(fmt.Sprintf("format2:%v/%v", path, format))
+	cacheKey := []byte(fmt.Sprintf("format5:%v/%v/%v", path, format, gzipContent))
 	if cached := s.cache.Get(ctx, cacheKey); cached != nil {
-		contentType = string(cached[:bytes.IndexByte(cached, 0)])
+		zipped := cached[0] == 1
+		contentType = string(cached[1:(bytes.IndexByte(cached[1:], 0) + 1)])
 		content := cached[150:]
-		return content, contentType, nil
+		return content, contentType, zipped, nil
 	}
 
 	// get media
 	content, contentType, err = s.Get(ctx, path)
 	if err != nil {
-		return nil, "", err
+		return nil, "", false, err
 	}
 
 	// remove whatever comes after @
@@ -47,7 +49,7 @@ func (s *MediaStore) GetFormattedMedia(ctx context.Context, path string, format 
 	format = formatParts[0]
 
 	// format media
-	if format != "" && format != "original" {
+	if format != "" && format != "original" && format != "raw" {
 		unknownFormat := true
 
 		// [width]x[height]
@@ -66,44 +68,63 @@ func (s *MediaStore) GetFormattedMedia(ctx context.Context, path string, format 
 				unknownFormat = false
 				content, contentType, err = imagekit.Fit(content, int(width), int(height), maxSize)
 				if err != nil {
-					return nil, "", err
+					return nil, "", false, err
 				}
 			}
 		}
 
 		if unknownFormat {
-			return nil, "", errors.New("no format specified")
+			return nil, "", false, errors.New("no format specified")
+		}
+	}
+
+	// check if we should zip
+	zipped = false
+	if gzipContent {
+		w := bytes.NewBuffer(nil)
+		compressor := gzip.NewWriter(w)
+		compressor.Write(content)
+		compressor.Close()
+		if w.Len() < len(content) {
+			zipped = true
+			content = w.Bytes()
 		}
 	}
 
 	// save to cache
 	serialized := make([]byte, 150+len(content))
-	copy(serialized, []byte(contentType))
+	if zipped {
+		serialized[0] = 1
+	} else {
+		serialized[0] = 0
+	}
+	copy(serialized[1:], []byte(contentType))
 	copy(serialized[150:], content)
 	s.cache.Set(ctx, cacheKey, serialized, time.Hour*24*30)
 
-	return content, contentType, nil
+	return content, contentType, zipped, nil
 }
 
-func (s *MediaStore) ServeMedia(ctx context.Context, path string, format string, w http.ResponseWriter, r *http.Request) {
+func (s *MediaStore) ServeMedia(ctx context.Context, path string, format string, w http.ResponseWriter, r *http.Request, allowGzipping bool) {
 	ctx, done := logkit.Operation(ctx, "servemedia", logkit.String("path", path), logkit.String("format", format))
 	defer done()
+
+	var zipped = allowGzipping && strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
 
 	// get the media
 	var content []byte
 	var contentType string
 	var err error
-	if format == "" || format == "raw" {
-		content, contentType, err = s.Get(ctx, path)
-		if err != nil {
-			logkit.Error(ctx, "Error getting formatted media", logkit.Err(err))
-		}
-	} else {
-		content, contentType, err = s.GetFormattedMedia(ctx, path, format)
-		if err != nil {
-			logkit.Error(ctx, "Error getting formatted media", logkit.Err(err))
-		}
+	//content, contentType, zipped, err = s.GetWithZip(ctx, path, zipped)
+	//if err != nil {
+	//logkit.Error(ctx, "Error getting formatted media", logkit.Err(err))
+	//}
+	//} else {
+	content, contentType, zipped, err = s.GetFormattedMedia(ctx, path, format, zipped)
+	if err != nil {
+		logkit.Error(ctx, "Error getting formatted media", logkit.Err(err))
 	}
+	//}
 
 	// Not found?
 	if content == nil || len(content) == 0 {
@@ -111,26 +132,12 @@ func (s *MediaStore) ServeMedia(ctx context.Context, path string, format string,
 		return
 	}
 
-	// Gzipping
-	/*if !strings.Contains(c.Request.Header.Get("Accept-Encoding"), "gzip") {
-		in := bytes.NewBuffer(imageBytes)
-		zipper, err := gzip.NewReader(in)
-		if err != nil {
-			c.ServerError(err.Error(), 500)
-		}
-		defer zipper.Close()
-
-		out := new(bytes.Buffer)
-		io.Copy(out, zipper)
-		zipper.Close()
-
-		imageBytes = buf.Bytes()
-	} else {
-		c.Header().Set("Content-Encoding", "gzip")
-	}*/
-
 	// Write response
+	if zipped {
+		w.Header().Set("Content-Encoding", "gzip")
+	}
 	w.Header().Set("Content-Type", contentType)
+	w.Header().Set("Content-Length", fmt.Sprintf("%v", len(content)))
 	w.Header().Set("Cache-Control", "public, max-age=31556926")
 	w.Header().Set("Expires", time.Now().AddDate(1, 0, 0).Format(http.TimeFormat))
 	w.WriteHeader(200)
@@ -140,6 +147,27 @@ func (s *MediaStore) ServeMedia(ctx context.Context, path string, format string,
 func (s *MediaStore) Get(ctx context.Context, path string) (content []byte, contentType string, err error) {
 	return s.underlying.Get(ctx, path)
 }
+
+/*func (s *MediaStore) GetWithZip(ctx context.Context, path string, gzipContent bool) (content []byte, contentType string, zipped bool, err error) {
+	content, contentType, err = s.underlying.Get(ctx, path)
+	if err != nil {
+		return content, contentType, false, err
+	}
+
+	zipped = false
+	if gzipContent {
+		w := bytes.NewBuffer(nil)
+		compressor := gzip.NewWriter(w)
+		compressor.Write(content)
+		compressor.Close()
+		if w.Len() < len(content) {
+			zipped = true
+			content = w.Bytes()
+		}
+	}
+
+	return content, contentType, zipped, err
+}*/
 
 func (s *MediaStore) Put(ctx context.Context, path string, contentType string, content []byte) error {
 	return s.underlying.Put(ctx, path, contentType, content)
