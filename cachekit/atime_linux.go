540ab102a5724621a8606784e7cd5f2bf9e32eb0
Oliver <oliver@oliverkofoed.com>
2021-08-03 20:59:47 +0200

    cachekit: fixing atime for linux (broke it when fixing atime for darwin)

--- cachekit/atime_linux.go
+++ cachekit/atime_linux.go
@@ -8,5 +8,5 @@ import (
 
 func atime(info os.FileInfo) time.Time {
 	stat := info.Sys().(*syscall.Stat_t)
-	return time.Unix(int64(stat.Atimespec.Sec), int64(stat.Atimespec.Nsec))
+	return time.Unix(int64(stat.Atim.Sec), int64(stat.Atim.Nsec))
 }
